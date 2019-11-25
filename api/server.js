fd8603a80f64167fdc3e41296c37b5cba60b415b
Essi Norri <essi.norri@outlook.com>
2019-11-25 20:14:07 +0200

    packages and then some

---
+++ api/server.js
@@ -0,0 +1,31 @@
+//http://127.0.0.1:8081/
+
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors());
+
+var mysql = require('mysql');
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "olso",
+    password: "olso",
+    database: "animals"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+});
