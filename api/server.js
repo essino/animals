e08f0b68308ff9639b541fe08efda2ae37d4111b
Essi Norri <essi.norri@outlook.com>
2019-11-28 13:07:01 +0200

    to

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-//http://127.0.0.1:8081/
+//git
 
 var express = require('express');
 var app = express();
@@ -8,22 +8,45 @@ app.use(cors());
 
 var mysql = require('mysql');
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+app.get('/cutiepie', function (req, res) {
+    console.log(req.params);
+
+    con.query("SELECT elain FROM Elainkuvia", function (err, result, fields) {
+        if (err) throw err;
+        let results = [];
+        if (result.length){
+            for (var i = 0; i < result.length; i++){
+                results.push(result[i]);
+            }
+            console.log(result);
+        }
+        //sends the HTTP response
+        res.send(results);
+    })
+});
+
+app.post('/table', function(req, res) {
+    console.log(req.body);
+    console.log(req.params);
+    con.query("INSERT INTO elainkuva (elain, kuvaus, linkki) VALUES ('lehmä', 'kaunis', 'https://upload.wikimedia.org/wikipedia/commons/3/32/SalersBreed_Cow_5.JPG');", function (err, result, fields) {
+        if (err) throw err;
+    })
+    res.send(req.body);
+});
 
 var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
 
     console.log("Example app listening at http://%s:%s", host, port)
 })
 
+//Cisco päälle?
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "",
-    password: "",
-    database: "animals"
+    host: "mysql.metropolia.fi",
+    user: "essinor",
+    password: "kissa",
+    database: "essinor"
 });
 
 con.connect(function(err) {
